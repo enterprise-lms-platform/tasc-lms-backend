de7e742de604662d5950bac0f082c1c3b3fda408
Peter Kakuru Bingwa <pkakuru@gmail.com>
2026-02-04 12:02:55 +0300

    feat/auth: cleaning up whiteNoise setting

--- config/settings.py
+++ config/settings.py
@@ -59,9 +59,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "allauth.account.middleware.AccountMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
-]
+    ]
 
 ROOT_URLCONF = "config.urls"
 
