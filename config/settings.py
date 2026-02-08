2ec527a7c16b2f8a7e8c6532c384a990638d9d0d
Peter Kakuru Bingwa <pkakuru@gmail.com>
2026-02-08 16:22:26 +0300

    fix: configure STATIC_ROOT for collectstatic in staging

--- config/settings.py
+++ config/settings.py
@@ -171,7 +171,9 @@ USE_TZ = True
 # ----------------------------------------
 # Static files
 # ----------------------------------------
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 
 # ----------------------------------------
 # REST framework
